a5465fd1c5cb4fa615d4238f0ddb90193740b493
Ilya <amidamaru1510@gmail.com>
2021-11-07 22:26:45 +0300

    Доделал выбор региона

--- start/js/main.js
+++ start/js/main.js
@@ -1,10 +1,11 @@
 'use strict';
-function ShowMore(){
+
+function ShowMore() {
   let dots = document.getElementById('dots');
   let moreText = document.querySelector(".services__content-text--more");
   let btnText = document.getElementById("btnMore");
-  
-  if (dots.style.display === 'none'){
+
+  if (dots.style.display === 'none') {
     dots.style.display = 'inline';
     btnText.innerHTML = "Читать полностью";
     moreText.style.display = 'none';
@@ -20,31 +21,38 @@ let modal = document.querySelector(".modal");
 let modal_content = document.querySelector('.modal-content');
 
 // Получить элемент <span>, который закрывает модальный
-let span = document.getElementsByClassName("close")[0];
+let span__header = document.getElementsByClassName("close__model-header")[0];
 let fault = document.querySelector('.modal__footer-link--n');
 
-// Когда пользователь нажимает на кнопку, откройте модальный
+// Когда пользователь открывает страницу, откройте модальный
 window.onload = function() {
-  modal.style.display = "flex";
+  if (localStorage.getItem('region') == null){
+    modal.style.display = "flex";
+  } else {
+    let region_link = document.querySelector('.region-link');
+    region_link.innerHTML = localStorage.getItem('region');
+  }
 };
+
 
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
-span.onclick = function() {
+span__header.onclick = function () {
   modal.style.display = "none";
 };
 
-fault.onclick = function() {
+function ModalNo() {
   modal.style.display = "none";
+  let region_link = document.querySelector('.region-link');
+  region_link.innerHTML = 'Не выбрано';
+
 };
 
 // Когда пользователь щелкает в любом месте за пределами модального, закройте его
-
-document.addEventListener('click', function(event) {
-  
-  if (!modal.contains(event.target)) modal.style.display='none';
+document.addEventListener('click', function (event) {
+  if (!modal.contains(event.target)) modal.style.display = 'none';
 });
 
-$(function(){
+$(function () {
 
   $('.slider__inner').slick({
     arrows: false,
@@ -55,3 +63,45 @@ $(function(){
   });
 
 });
+
+
+let modal__choose = document.querySelector('.modal__choose');
+let span__choose = document.querySelector('.close__choose');
+
+
+//появление окна выбора региона
+function ShowModalRegion() {
+  modal__choose.style.display = 'block';
+  modal.style.display = "none";
+
+};
+
+//закрытие по крестику
+span__choose.onclick = function () {
+  modal__choose.style.display = 'none';
+};
+
+
+let modal__ch = document.querySelector('.modal__choose');
+window.onclick = function(event) {
+  if (event.target == modal__choose) {
+    if (localStorage.getItem('region') == null){
+      ModalNo();
+      modal__ch.style.display = "none";
+
+    } else {
+      modal__ch.style.display = "none";
+    }
+    
+  }
+};
+
+
+function getValue() {
+  var select = document.getElementById("region");
+  var value = select.value;
+  let region_link = document.querySelector('.region-link');
+  region_link.innerHTML = value;
+  localStorage.setItem('region', value.toString());
+
+}
